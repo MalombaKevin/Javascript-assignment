8896d4e614b5c57efd84338d815dd9abdb0842e4
Kevin Malomba <malombakevin@gmail.com>
2023-01-20 02:38:51 +0300

    initial commit

---
+++ forIn.js
@@ -0,0 +1,9 @@
+
+
+let teams = ['Arsenal', 'Mancity', 'Chelsea', 'Manchester United']
+
+for ( i in teams){
+    console.log(teams[i]);
+
+}
+
---
+++ recursion.js
@@ -0,0 +1,18 @@
+// Recursion function that solves the Factorial of any number
+
+// program to find the factorial of a number
+function factorial(x) {
+
+    // if number is 0
+    if (x == 0) {
+        return 1;
+    }
+
+    // if number is positive
+    else {
+        return x * factorial(x - 1);
+    }
+}
+
+console.log (factorial(5))
+
